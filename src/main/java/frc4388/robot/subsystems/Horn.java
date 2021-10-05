2f5445ee860b5db20a931c7da0591891ebeb6703
Aarav Shah <76842@psdschools.org>
2021-10-04 18:16:13 -0600

    Create Horn.java

---
+++ src/main/java/frc4388/robot/subsystems/Horn.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc4388.robot.subsystems;
+
+package edu.wpi.first.wpilibj;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Horn extends SubsystemBase {
+  /**
+   * Creates a new Horn.
+   */
+  public Horn() {
+    Hell
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
